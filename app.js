69b8ea45aa6c9dc0327f4b3d6dd79a291d90c01d
luanpoppe <118562068+luanpoppe@users.noreply.github.com>
2024-02-11 15:54:25 -0300

    Terminei a aula 05-02 - Speech

--- app.js
+++ app.js
@@ -6,11 +6,14 @@ let tentativas = 1
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {
+        rate: 1.2
+    })
 }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10 vai tomar no cu');
 }
 
 exibirMensagemInicial()
@@ -19,7 +22,7 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', "Acertou!")
-        let palavraTentativa = tentativas > 1 ? "tentativa" : "tentativas"
+        let palavraTentativa = tentativas > 1 ? "tentativas" : "tentativa"
         let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`
         exibirTextoNaTela('p', mensagemTentativas)
         document.getElementById('reiniciar').removeAttribute('disabled')
