146a71c7808f966c3d97a43eb2d5b12f2392d61c
luanpoppe <118562068+luanpoppe@users.noreply.github.com>
2024-02-11 15:28:39 -0300

    Terminei a parte 0 do curso

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ function exibirTextoNaTela(tag, texto) {
     campo.innerHTML = texto;
 }
 
-exibirTextoNaTela('h1', 'Jogo do número secreto');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial()
 
 function verificarChute() {
     let chute = document.querySelector('input').value;
@@ -35,4 +39,12 @@ function gerarNumeroAleatorio() {
 function limparCampo() {
     chute = document.querySelector('input')
     chute.value = ''
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio()
+    limparCampo()
+    tentativas = 1
+    exibirMensagemInicial()
+    document.getElementById('reiniciar').setAttribute('disabled', true)
 }
